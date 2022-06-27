e66d93c903073b2a490eb117010ea1e3da878a6f
Лазарович Виталий Владимирович <VLazarovich@alfabank.ru>
2022-06-27 18:12:03 +0300

    redux toolkit lesson A

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import './App.css';
 import CssBaseline from '@mui/material/CssBaseline';
-import TourCard from 'components/TourCard';
 import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
 import SearchAppBar from 'components/AppBar';
-import cities from 'data.json';
-import Typography from '@mui/material/Typography';
-import MyLink from './components/my-link';
-
 
 import getNumbersTo1Dmg from 'engine/pummel'
-
+import {userSlice} from 'store/reducers/UserSlice';
+import {useAppDispatch, useAppSelector} from 'utils/hooks/redux';
 
 function App() {
+    const {count} = useAppSelector(state => state.userReducer)
+    const {increment} = userSlice.actions;
+    const dispatch  = useAppDispatch();
 
     React.useEffect(()=>{
         getNumbersTo1Dmg();
@@ -26,6 +24,13 @@ function App() {
         <SearchAppBar/>
         <Container maxWidth="lg" sx={{marginY: 5}}>
             {/*https://www.youtube.com/watch?v=o1chMISeTC0*/}
+
+
+            {/*https://youtu.be/Od5H_CiU2vM?t=685*/}
+            <h1>{count}</h1>
+            <br/>
+            <button onClick={() => dispatch(increment(5))}>button</button>
+            <br/>
 
         </Container>
     </React.Fragment>
